1292d42e1ed40c3247f568b8c719f37c1b7cb852
Moustafa Mansour <moustafa186@gmail.com>
2019-02-11 00:16:06 +0100

    use lombok annotation to remove boilerplate code

--- src/main/java/com/outfittery/AppointmentManager/stylists/StylistService.java
+++ src/main/java/com/outfittery/AppointmentManager/stylists/StylistService.java
@@ -1,17 +1,15 @@
 package com.outfittery.AppointmentManager.stylists;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class StylistService {
 
     @Autowired
     private StylistRespository stylistRespository;
-
-    public StylistService(StylistRespository stylistRespository) {
-        this.stylistRespository = stylistRespository;
-    }
 
     public Stylist createStylist(Stylist stylist) {
         return stylistRespository.save(stylist);
