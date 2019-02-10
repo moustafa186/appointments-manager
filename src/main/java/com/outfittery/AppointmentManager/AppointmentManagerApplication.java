46cefa559c7395f39c07dd889331b9a3f80dbba7
Moustafa Mansour <moustafa186@gmail.com>
2019-02-10 18:56:35 +0100

    show spring version

--- src/main/java/com/outfittery/AppointmentManager/AppointmentManagerApplication.java
+++ src/main/java/com/outfittery/AppointmentManager/AppointmentManagerApplication.java
@@ -2,11 +2,13 @@ package com.outfittery.AppointmentManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class AppointmentManagerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring version: " + SpringVersion.getVersion());
 		SpringApplication.run(AppointmentManagerApplication.class, args);
 	}
 
