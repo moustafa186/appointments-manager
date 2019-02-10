b353cb2ca19fe3a4d4daf0bfa0a7fbcb6a084014
Moustafa Mansour <moustafa186@gmail.com>
2019-02-10 18:00:43 +0100

    Initial commit

---
+++ src/main/java/com/outfittery/AppointmentManager/AppointmentManagerApplication.java
@@ -0,0 +1,14 @@
+package com.outfittery.AppointmentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentManagerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/outfittery/AppointmentManager/AppointmentManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.outfittery.AppointmentManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppointmentManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
